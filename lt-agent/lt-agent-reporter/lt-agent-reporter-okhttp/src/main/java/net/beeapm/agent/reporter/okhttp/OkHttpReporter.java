5f6616ddea7911e18d263006bc25cf62e829a1e2
LT Monitor Dev <dev@xi.org>
2026-04-01 22:05:54 +0800

    fix: 修复lt-agent模块所有编译错误，全模块编译成功

--- lt-agent/lt-agent-reporter/lt-agent-reporter-okhttp/src/main/java/net/beeapm/agent/reporter/okhttp/OkHttpReporter.java
+++ lt-agent/lt-agent-reporter/lt-agent-reporter-okhttp/src/main/java/net/beeapm/agent/reporter/okhttp/OkHttpReporter.java
@@ -1,35 +1,49 @@
 package org.xi.lt.agent.reporter.okhttp;
 
-import com.alibaba.fastjson.JSON;
 import org.xi.lt.common.annotation.LtPlugin;
 import org.xi.lt.common.annotation.LtPluginType;
 import org.xi.lt.agent.model.Span;
+import org.xi.lt.common.utils.HttpUtils;
+import org.xi.lt.common.utils.JsonUtils;
 import org.xi.lt.agent.reporter.AbstractReporter;
 
 import java.util.List;
 
 /**
- * @author yuan
- * @date 2018/08/30
+ * HTTP上报实现类
+ * 异步批量上报Span数据到服务端，支持失败重试
+ * @author LT Monitor Dev
+ * @date 2026/04/01
  */
-@LtPlugin(type = LtPluginType.REPORTER, name = "kafka")
+@LtPlugin(type = LtPluginType.REPORTER, name = "okhttp")
 public class OkHttpReporter extends AbstractReporter {
     public String name = "okhttp";
 
+    /**
+     * 上报服务端地址，从Agent参数获取
+     */
+    private String serverUrl;
+
     @Override
     public int report(Span span) {
-        OkHttpHelper.getInstance().post(JSON.toJSONString(span));
+        if (serverUrl != null) {
+            HttpUtils.asyncPostJson(serverUrl, JsonUtils.toJsonString(span));
+        }
         return 1;
     }
 
     @Override
     public int report(List<Span> list) {
-        OkHttpHelper.getInstance().post(JSON.toJSONString(list));
+        if (serverUrl != null && !list.isEmpty()) {
+            HttpUtils.asyncPostJson(serverUrl, JsonUtils.toJsonString(list));
+        }
         return list.size();
     }
 
     @Override
     public int init() {
+        // 从配置中获取上报地址
+        serverUrl = System.getProperty("lt.agent.report.url", "http://127.0.0.1:8080/apm/report");
         return 0;
     }
 }

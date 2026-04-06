055e6b89306fd48dcde6a37e8646a3c99d3d8a78
moriwuzhe <moriwuzhe@users.noreply.github.com>
2026-04-06 04:35:19 +0000

    feat: 优化功能列表
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- lt-ui/bee-apm-ui-web/src/ui/layouts/AppLayout.vue
+++ lt-ui/bee-apm-ui-web/src/ui/layouts/AppLayout.vue
@@ -23,7 +23,11 @@ onMounted(() => {
       </header>
 
       <main class="app-content">
-        <RouterView />
+        <RouterView v-slot="{ Component, route }">
+          <KeepAlive include="DashboardView,RequestView,MethodView,SqlView,TxView,LoggerView,AppListView">
+            <component :is="Component" :key="route.fullPath" />
+          </KeepAlive>
+        </RouterView>
       </main>
     </div>
   </div>
